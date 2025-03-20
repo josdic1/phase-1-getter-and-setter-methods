6bd9adfa81b91846a673457217e8650bd5ffcb1b
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-20 12:47:01 -0400

    circle lab done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your Circle class here
+const pi = Math.PI
+
+class Circle {
+
+   constructor(radius) {
+      this.radius = radius
+   }
+
+   set diameter(diameter) {
+      this.radius = diameter / 2;
+   }
+
+   get diameter() {
+      return this.radius * 2;
+   }
+
+   set circumference(circumference) {
+      this.radius = circumference / (2 * pi)
+   }
+
+   get circumference() {
+      return circle.diameter * pi
+   }
+
+   set area(area) {
+      this.radius = Math.sqrt(area / pi)
+   }
+
+   get area() {
+      return this.radius ** 2 * pi
+   }
+
+
+}
+
+const circle = new Circle(6)
+
+
+
+
